59b1635f593b41e7d7f91dce5e59ad62d08154b7
stepheeeen <stephenon56@gmail.com>
2023-12-19 01:39:15 -0800

    qs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,18 @@ import heroImg from '../assets/hero.png'
 function Hero() {
     return(
         <div className='hero'>
+            <div>
+            <h1>
+                transforming finance
+                <br />
+                with innovation
+            </h1>
+
+            <p>
+
+            </p>
+            </div>
+
             <img src={heroImg} alt="image" className='hero-img'/>
         </div>
     )
