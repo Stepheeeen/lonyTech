04b37408fbf1dacf81f6623f36e933cbba80f681
stepheeeen <stephenon56@gmail.com>
2023-12-19 07:42:37 -0800

    mek

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -6,8 +6,15 @@ function Service() {
             <div className='service'>
                 <div className='description'>
                     <h1>
-
+                        We Elevate Your Finances:
                     </h1>
+                    <p>
+                        Experience the Future with Lonytech's Cutting-Edge Solutions.
+                    </p>
+
+                    <a href="#">
+                        Explore Paylony Mobile App
+                    </a>
                 </div>
             </div>
         </>
