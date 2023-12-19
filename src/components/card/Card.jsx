b0c8380f0c9f947ef77fb8ae3058599df4724090
stepheeeen <stephenon56@gmail.com>
2023-12-19 06:53:45 -0800

    rg

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,22 @@
+import '../../App.css'
+import fintech from '../../assets/fintech.png'
+
+function Card() {
+    return(
+        <div className='light-card'>
+            <div className='description'>
+                <h1>
+
+                </h1>
+
+                <p>
+
+                </p>
+            </div>
+
+        <img src={fintech} alt="" />
+        </div>
+    )
+}
+
+export default Card
