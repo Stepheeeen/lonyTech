7a89cedeaa49b18f9bd26d7e29a808cb0ad4252d
stepheeeen <stephenon56@gmail.com>
2023-12-19 06:56:00 -0800

    qwdA

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,19 +2,19 @@ import '../../App.css'
 import fintech from '../../assets/fintech.png'
 
 function Card() {
-    return(
+    return (
         <div className='light-card'>
             <div className='description'>
                 <h1>
-
+                    We are a trailblazing fintech company.
                 </h1>
 
                 <p>
-
+                    Leveraging experienced software engineers and cutting-edge infrastructure.
                 </p>
             </div>
 
-        <img src={fintech} alt="" />
+            <img src={fintech} alt="" />
         </div>
     )
 }
