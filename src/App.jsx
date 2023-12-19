69287528d6afbd76bf1c434f29b90db8706f0725
stepheeeen <stephenon56@gmail.com>
2023-12-19 05:56:50 -0800

    vv

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,31 @@ import './App.css'
 import logo from './assets/logo.png'
 import Hero from './components/Hero.jsx'
 import Button from './components/button/Button.jsx'
+import marquee from 'react-fast-marquee'
 
 function App() {
 
   return (
     <>
-    <nav className='navbar'>
-    <img src={logo} alt='logo' className='logo' />
+      <nav className='navbar'>
+        <img src={logo} alt='logo' className='logo' />
 
-    <ul className='nav-links'>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Product</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
+        <ul className='nav-links'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Product</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
 
-    <Button message='Learn More' />
-    </nav>
+        <Button message='Learn More' />
+      </nav>
 
-    <main>
-    <Hero />
+      <main>
+        <Hero />
 
 
-    </main>
+
+      </main>
     </>
   )
 }
