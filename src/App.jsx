7b7eee5174d40276f828c063b2a0ef1e9b13c48f
stepheeeen <stephenon56@gmail.com>
2023-12-19 01:24:56 -0800

    jb

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,11 @@ function App() {
     </button>
     </nav>
 
+    <main>
     <Hero />
+
+    
+    </main>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import '../App.css'
 
 function Hero() {
     return(
-        <div>
+        <div className='hero'>
 
         </div>
     )
